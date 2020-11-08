8e3ddaacb9441ce49f7368a6a13a088e36f1cac8
Fixer1 <figat.wcale.nie@gmail.com>
2020-11-08 22:06:27 +0000

    b

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -6,7 +6,8 @@ class ResourceManager
 {
   Resource* wskaznik;
   public:
-    ResourceManager(){wskaznik=new Resource;}
+    wskaznik=new Resource;
+    ResourceManager():wskaznik{}{}
     ~ResourceManager() {delete wskaznik;}
     double get() { return wskaznik[0].get(); }
     ResourceManager(const ResourceManager& res):wskaznik{res.wskaznik}{};
